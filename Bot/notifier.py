5d0c416e15713aced659690bdcf562ead0d2d76e
sandeshkumatgi <kumatgisandesh@gmail.com>
2025-07-25 21:30:00 +0530

    Case Sensetivity

--- Bot/notifier.py
+++ Bot/notifier.py
@@ -5,7 +5,7 @@ from db import get_users_by_pair
 API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 def send_arbitrage_alert(pair, message):
-    subs = get_users_by_pair(pair)
+    subs = get_users_by_pair(pair.lower())
     for user in subs:
         payload = {"chat_id": user["chat_id"], "text": message}
         requests.post(API_URL, data=payload)
